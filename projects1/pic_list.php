ca918d046950c46ec4b9880aa5def740c096805e
Ben Shyong <likeachild@gmail.com>
2012-10-02 01:20:35 -0400

    added image carousel fixes and image array

---
+++ projects1/pic_list.php
@@ -0,0 +1,20 @@
+      <?php 
+        $current_dir = "$DOCUMENT_ROOT"."Quotes/";    //Put in second part, the directory - without a leading slash but with a trailing slash! 
+        $dir = opendir($current_dir);        // Open the directory
+        $json = '{"pictures":[';
+
+        while ($file = readdir($dir))            // while loop 
+          { 
+          $parts = explode(".", $file);                    // pull apart the name and dissect by period 
+          if (is_array($parts) && count($parts) > 1) {    // does the dissected array have more than one part 
+              $extension = end($parts);        // set to we can see last file extension 
+              if ($extension == "jpg" OR $extension == "JPG")    // is extension jpg or JPG ? 
+                $json = $json.'{"url":"http://www.labtpe.com/Quotes/'.urlencode($file).'"},';
+            } 
+          } 
+
+          $json = $json.'{"url":null}]}';
+
+        closedir($dir);        // Close the directory after we are done 
+        echo $json;
+      ?>
